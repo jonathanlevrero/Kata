d84f32265a16d7adb931fca01cec6d4f12025179
jonathanlevrero <jonathan.levrero@gmail.com>
2020-02-10 14:46:12 -0300

    Kata1

---
+++ Ejercicio.js
@@ -0,0 +1,7 @@
+function lista(lista){
+    let listaDuplicada = []
+    for (i=0; i<lista.lenght; i++){
+        listaDuplicada= lista [i] * 2;
+    }
+    return listaDuplicada;
+    console.log(listaDuplicada);
