f49e5716028548bb31ec55062da75dfb1946ff36
jonathanlevrero <jonathan.levrero@gmail.com>
2020-02-10 16:17:23 -0300

    Kata3

--- Ejercicio.js
+++ Ejercicio.js
@@ -1,8 +1,5 @@
-function lista(lista){
-    let listaDuplicada = []
-    for (i=0; i<lista.lenght; i++){
-        listaDuplicada= lista [i] * 2;
-    }
-    return listaDuplicada;
-    console.log(listaDuplicada);
-}
+
+function DiasRestantes (DiasTranscurridos,DiasTotales) {
+  return (DiasTotales - DiasTranscurridos)}
+
+
