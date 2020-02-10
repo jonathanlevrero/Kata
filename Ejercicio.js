76fca76d77a9f8f818638c67f0ee37d38ef0dadd
jonathanlevrero <jonathan.levrero@gmail.com>
2020-02-10 15:25:11 -0300

    Kata2 corregido

--- Ejercicio.js
+++ Ejercicio.js
@@ -1,8 +1,4 @@
-function lista(lista){
-    let listaDuplicada = []
-    for (i=0; i<lista.lenght; i++){
-        listaDuplicada= lista [i] * 2;
-    }
-    return listaDuplicada;
-    console.log(listaDuplicada);
-}
+var dia = 10;
+var mes = 02;
+var año = 2020
+console.log (dia, mes, año)
